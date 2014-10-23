25b1b89397e9e7d2ba7e80304bb414a48b7f9670
Jan Ivar Beddari <code@beddari.net>
2014-10-23 12:29:30 +0200

    rpm DASH build ... is the correct package

--- nav-recipe/fpm-cookery.sh
+++ nav-recipe/fpm-cookery.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-yum -y install epel-release rpmbuild
+yum -y install epel-release rpm-build
 
 wget -O ruby-install-0.5.0.tar.gz https://github.com/postmodern/ruby-install/archive/v0.5.0.tar.gz
 tar -xzvf ruby-install-0.5.0.tar.gz
